4336d312d14c7b6a9b211132f5323129adc8ff38
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-05-16 10:27:29 -0400

    requestBody

---
+++ first try/requestBody.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Mon(BaseModel):
+    name: str
+    description: str | None = None
+    attack: float
+    defense: float | None = None
+    speed: float
+
+
+app = FastAPI()
+
+
+@app.post("/mons/")
+async def create_item(mon: Mon):
+    
+    mon_dict = mon.model_dump()
+    if mon.defense is not None:
+        total_defense = mon.attack + mon.defense
+        mon_dict.update({"total_defense": total_defense})
+    return mon_dict
